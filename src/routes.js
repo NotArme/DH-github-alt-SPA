fdde6c019529ac02f7dbaf716c174183c35c3126
Alice Marques <alice_marquesa@hotmail.com>
2021-10-05 11:31:40 -0300

    ...

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import Error404 from './pages/Errors/Error404';
 import Disabled from './pages/Errors/Disabled';
+import Pesquisa from "./pages/pesquisa";
 
 function Routes() {
   return (
     <div className="container-lg">
       <Switch>
-
+        <Route path="/pesquisa" component={Pesquisa} />
         <Route path='/contact' component={Disabled} />
         <Route component={Error404} />
       </Switch>
