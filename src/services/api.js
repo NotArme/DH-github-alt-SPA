05d98741b114faae10ae57eab769b394d89064df
Alice Marques <alice_marquesa@hotmail.com>
2021-10-05 14:23:01 -0300

    funcionando

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import axios from "axios"
-
-const api = axios.create({
-    baseURL: "https://api.github.com",
-    headers: {
-        Authorization: "token " + process.env.REACT_APP_TOKEN
-    }
-})
-
-export default api; 
-=======
 import axios from 'axios';
 
 const api = axios.create({
@@ -20,4 +8,3 @@ const api = axios.create({
 });
 
 export default api;
->>>>>>> 8b9ed9da3eb63e45a23705eb817eb6c8895b1472
